788f3765637185f93fbcd873700b30004a48dd17
foggy <1171693744@qq.com>
2026-04-29 12:19:36 +0800

    修改

--- src/main/java/com/qingmei/reviewplatform/config/WebConfig.java
+++ src/main/java/com/qingmei/reviewplatform/config/WebConfig.java
@@ -23,7 +23,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://127.0.0.1:5173",
+                                "https://qm.upcshare.cn",
+                                "https://qm.upcshare.cn:8088"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("Origin", "Content-Type", "Authorization")
                         .allowCredentials(true)
